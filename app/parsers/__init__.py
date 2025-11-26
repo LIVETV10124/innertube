8ac65cce0431a46988236f39e406d43debfc5113
LIVETV10124 <salinadabvg@gmail.com>
2025-11-27 01:54:08 +0600

    Initialize parsers module with imports

---
+++ app/parsers/__init__.py
@@ -0,0 +1,5 @@
+from app.parsers.youtube import YouTubeParser
+from app.parsers.music import MusicParser
+from app.parsers.stream import StreamParser
+
+__all__ = ["YouTubeParser", "MusicParser", "StreamParser"]

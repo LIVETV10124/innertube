da59a6ba8e5809addf845d82d49b447e5cede311
LIVETV10124 <salinadabvg@gmail.com>
2025-11-27 01:47:45 +0600

    Create __init__.py

---
+++ app/api/v1/schemas/__init__.py
@@ -0,0 +1,11 @@
+from app.api.v1.schemas.common import APIResponse
+from app.api.v1.schemas.youtube import VideoResponse, ChannelResponse
+from app.api.v1.schemas.music import SongResponse, AlbumResponse
+
+__all__ = [
+    "APIResponse",
+    "VideoResponse", 
+    "ChannelResponse",
+    "SongResponse",
+    "AlbumResponse"
+]
